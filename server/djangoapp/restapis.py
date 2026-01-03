4c376611259a2a7d026b341675211936a6c51b6b
Youssef Werrad <youssefwerrad@gmail.com>
2026-01-03 12:20:08 -0500

    Add Dynamic Pages!

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -13,7 +13,6 @@ sentiment_analyzer_url = os.getenv(
 
 def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
-    def get_request(endpoint, **kwargs):
     params = ""
     if(kwargs):
         for key,value in kwargs.items():
