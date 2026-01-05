01bf9401da3806db095ac41a594ca6c425eef8c0
youssefwerrad <youssefwerrad@gmail.com>
2026-01-05 11:33:38 +0000

    Mon, Jan 5, 2026, 11:33 AM +00:00

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://youssefwerra-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://youssefwerra-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = ['https://youssefwerra-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
